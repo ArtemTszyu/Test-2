8bebc4b2734529097cbace71fafab84e3d9aa3df
ArtemTszyu <32194510+ArtemTszyu@users.noreply.github.com>
2017-11-13 15:58:31 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -26,19 +26,16 @@ bool str(float *st,int n)
     return true;
 }
 
-void reverse (float* st, int n, int m)
-{
-     for (int i = 0 ; i = ( n - m ) / 2 ; i++) {
-         swap(st[i], st[n-m-1-i]);
-     }
-     
-     for(int i=n-m, j=0; i < n - m + (m / 2); i++, j++) {
-          swap(st[i], st[n-i-1]);
-     }
-     
-     for(int i=0; i < ( n / 2 ); i++) {
-          swap(st[i], st[n-i-1]);
-     }
+void reverse(float *st, int n, int m) {
+	  for (int i = 0; i < (n - m) / 2; i++) {
+	    swap(st[i], st[n - m - i - 1]);
+	  }
+	  for (int i = n - m, j = 0; i < n - m + (m / 2); i++, j++) {
+	    swap(st[i], st[n - 1 - j]);
+	  }
+	  for (int i = 0; i < (n / 2); i++) {
+	    swap(st[i], st[n - 1 - i]);
+	  }
   }
 
 
@@ -53,6 +50,7 @@ void print_st(float * st,int n)
 int main() {
         int m;
         int n;
+        string strok;
             if(!(cin >> n)||(n<0)) {
         cout << "An error has occured while reading numbers from line";
         return 0;
@@ -60,17 +58,22 @@ int main() {
         
         cin.get();
         float *st = new float[n];
-        if (str(st, n)) {
-                            reverse(st, n, m);
-                            if(!(cin >> m)||(n<0)) {
-                            cout << "An error has occured while reading numbers from line";
-                            return 0;
-            print_st(st, n);
-        }
-        else {
-            cout << "An error has occured while reading numbers from line";
-        }
-        delete[] st;
-        return 0;
-    }
+       if (str(st, n)) {
+    		getline(cin, strok);
+    		istringstream stream(strok);
+   			if (!(stream >> m)) {
+     		cout << "An error has occurred while reading input data" << endl;
+    		return -1;
+    		}
+
+    	reverse(st, n, m);
+    	print_st(st, n);
+    	cin.get();
+ 		} 
+  else {
+    cout << "An error has occured while reading numbers from line";
+    cin.get();
+  }
+  delete[] st;
+  return 0;
 }
